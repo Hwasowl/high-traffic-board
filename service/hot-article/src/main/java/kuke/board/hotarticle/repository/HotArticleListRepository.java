1e6bf778f06f7bc07200ad2c31414245f8d8bf2b
Hwasowl <sound2339@naver.com>
2025-05-26 22:00:21 +0900

    feat: 인기글 레포지토리 구현

---
+++ service/hot-article/src/main/java/kuke/board/hotarticle/repository/HotArticleListRepository.java
@@ -0,0 +1,52 @@
+package kuke.board.hotarticle.repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class HotArticleListRepository {
+    private final StringRedisTemplate redisTemplate;
+
+    // hot-article::list::{yyyyMMdd}
+    private static final String KEY_FORMAT = "hot-article::list::%s";
+
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    public void add(Long articleId, LocalDateTime time, Long score, Long limit, Duration ttl) {
+        redisTemplate.executePipelined((RedisCallback<?>) action -> {
+            StringRedisConnection connection = (StringRedisConnection) action;
+            String key = generateKey(time);
+            connection.zAdd(key, score, String.valueOf(articleId));
+            connection.zRemRange(key, 0, -limit - 1);
+            connection.expire(key, ttl.toSeconds());
+            return null;
+        });
+    }
+
+    private String generateKey(LocalDateTime time) {
+        return generateKey(TIME_FORMATTER.format(time));
+    }
+
+    private String generateKey(String date) {
+        return String.format(KEY_FORMAT, date);
+    }
+
+    public List<Long> readAll(String key) {
+        return redisTemplate.opsForZSet()
+            .reverseRangeWithScores(generateKey(key), 0, -1).stream()
+            .peek(tuple -> log.info("[HotArticleListResponse.readAll] articleId={}, score={}", tuple.getValue(), tuple.getScore()))
+            .map(tuple -> Long.valueOf(tuple.getValue()))
+            .toList();
+    }
+}
